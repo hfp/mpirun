649004156cb9861d40b014b16a40515909291a11
Hans Pabst <hans.pabst@intel.com>
2014-12-08 10:23:56 +0100

    Cleanup

--- mpirun.sh
+++ mpirun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ###############################################################################
-## Copyright (c) 2013-2015, Intel Corporation                                ##
+## Copyright (c) 2014-2015, Intel Corporation                                ##
 ## All rights reserved.                                                      ##
 ##                                                                           ##
 ## Redistribution and use in source and binary forms, with or without        ##
